a34ced579d6b7c04adff0ffebb3eff3336e5721f
gamgoon <gamgoon@gmail.com>
2018-12-08 09:55:54 +0900

    first commit

---
+++ src/main/java/com/github/gamgoon/rest/RestApiInflearnApplication.java
@@ -0,0 +1,12 @@
+package com.github.gamgoon.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiInflearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiInflearnApplication.class, args);
+	}
+}
